209eb126409bd9066492638466e6fb9387a2b642
Anatolii Ivanov <gotnickname@gmail.com>
2023-05-17 06:30:26 +0530

    Add project files.

---
+++ SkillFactory.Task12_1_5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SkillFactory.Task12_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var user1 = new User() { Name = "Chad", Login = "Power", IsPremium = true };
+        }
+    }
+}
